ed86e0c0c7e8428d264a78b165f5ae253bc3dd71
Jason Lau <i@dotkrnl.com>
2026-06-21 18:39:27 -0700

    feat: render mdx math with katex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [[rehypeKatex, { output: "html" }]]
+    })
+  ],
   outDir: "_site-astro",
   srcDir: "src/app",
   publicDir: "public"
