af0b4ce34fc3ad86bf13ec83d668228edadae032
Jason Lau <i@dotkrnl.com>
2026-07-03 19:54:15 -0700

    feat: add day 010 frontier map interaction

--- src/lib/checks/content.ts
+++ src/lib/checks/content.ts
@@ -101,6 +101,7 @@ const WEB_ONLY_COMPONENTS = new Set([
   "CredenceDial",
   "Day9Interactive",
   "Day10IdealizationDial",
+  "Day10FrontierMap",
   "DemarcationLab",
   "DiscoveryPurityEngine",
   "DoSeeCalculator",
